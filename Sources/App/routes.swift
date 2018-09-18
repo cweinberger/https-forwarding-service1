ea2357b2ecfbd764ee9b1be69389f7606693b6a8
cweinberger <christian.weinberger@me.com>
2018-09-18 13:52:51 +0200

    add more routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,4 +6,14 @@ public func routes(_ router: Router) throws {
     router.get("hello") { req in
         return "Hello, world!"
     }
+
+    let baseURL = "https://chwe-service-2-development.vapor.cloud"
+
+    router.get("hello-forward") { req in
+        return try req.client().get("\(baseURL)/hello")
+    }
+
+    router.get("google-forward") { req in
+        return try req.client().get("\(baseURL)/google")
+    }
 }
